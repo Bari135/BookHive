6c74f80f555b9e5828aab5152819cb1f22d7c350
bari135 <baribez@post.bgu.ac.il>
2024-11-11 11:07:45 +0200

    added index.html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
-const { times } = require('ramda');
+const { times, uniq } = require('ramda');
 
 
 require('dotenv').config()
@@ -13,7 +13,7 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/index.html')
 });
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -31,12 +31,47 @@ const bookSchema = new Schema({
   followers
 });
 const userSchema = new Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
+    username: { type: String, required: true, unique: true},
+    password: { type: String },
+    email: { type: String, required: true, unique: true },
     books: { type: [bookSchema]}
   });
 
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        try {
+            const hash = await bcrypt.hash(this.password, SALT_ROUNDS);
+            this.password = hash;
+            next();
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        next();
+    }
+});
+
 const User = mongoose.model('User', userSchema);
-const Book = mongoose.model('Log', logSchema);
+const Book = mongoose.model('Book', bookSchema);
 const Note = mongoose.model('Note', noteSchema);
 
+app.post('/register', (req, res) => {
+    const { username, password, email } = req.body;
+    
+    const existingUser = User.findOne({email}, (err, existingUser) => {
+        if (err) return res.status(500).json({ error: 'Server error' });
+        if (existingUser) return res.status(400).json({ message: 'Email already in use' });
+        
+        const newUser = new User({ username, password, email });
+        newUser.save((err, user) => {
+            if (err) return res.status(500).json({ error: 'error saving user' });
+            res.status(201).json({ message: 'User registered successfully', userId: user._id });
+        });
+    });
+    
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+})
